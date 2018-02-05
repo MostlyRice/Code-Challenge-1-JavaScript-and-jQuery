bac4f129c41bf3a2fde98dc7a88c4b15fedef5e7
Zygy93 <amazinzygy@gmail.com>
2018-02-05 15:00:49 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log( 'js' );
+
+$( document ).ready( function(){
+  $('button').on('click',function(){
+        var div = document.createElement('div');
+        $("#container").append(div);
+        $('#container div').append('<p>placeholder</p>');
+        $('#container div').append($('<input type="button" value="Swap"/>'));
+        $('#container div').append($('<input type="button" value="Delete"/>'));
+  });
+  }); //end doc ready
